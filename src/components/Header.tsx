71f8181aa63b294c2e1114640dfa1e2167769411
ekaone <ekaone3033@gmail.com>
2021-03-27 20:58:01 +0700

    init

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Fragment, ReactChildren, FunctionComponent } from 'react'
+
+const Header: FunctionComponent = ({ children }: { children: ReactChildren }) => {
+    return (
+      <header className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          {children}
+      </header>
+    )
+}
+
+export default Header
